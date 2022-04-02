8d6fff02d13cf02a41c284492a653326bd833ca7
Sebastien Bailouni <sbailouni@gmail.com>
2022-04-02 14:12:18 +1100

    Initial Commit

---
+++ iosHifi/iosHifiApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosHifiApp.swift
+//  iosHifi
+//
+//  Created by Sebastien Bailouni on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct iosHifiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
